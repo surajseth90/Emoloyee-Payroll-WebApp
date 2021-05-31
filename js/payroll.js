9d0aaa5f5328c4ef8ce702d013193fc52425877d
surajseth90 <surajseth90@gmail.com>
2021-05-31 14:08:40 +0530

    Added employee payroll object

--- js/payroll.js
+++ js/payroll.js
@@ -4,3 +4,13 @@ output.textContent = salary.value;
 salary.addEventListener('input', function() {
     output.textContent = salary.value;
 });
+
+document.getElementById("submit").onclick = function() {
+    let employee = new EmployeePayroll();
+    employee._name = document.getElementById("name").value;
+    employee._picture=document.getElementsByClassName("profile").value;
+    employee._gender=document.getElementsByClassName("gender").value;
+    employee._department = document.querySelector('input[name = department]:checked').value;
+    employee._salary = document.getElementById("salary").value;
+    employee._notes = document.getElementById("notes").value;
+};
