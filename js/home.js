9bc53b21f77cbbcaac4da6d2085ae3cf4500a23f
surajseth90 <surajseth90@gmail.com>
2021-06-07 13:16:49 +0530

    Added code to retreive data from json server

--- js/home.js
+++ js/home.js
@@ -1,16 +1,38 @@
 let employeePayrollList;
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    employeePayrollList = getEmployeePayrollDataFromStorage();
+    if(site_properties.use_local_storage.match("true")) {
+        getEmployeePayrollDataFromStorage();
+    } else getEmployeePayrollDataFromServer();
+    
+});
+
+
+const getEmployeePayrollDataFromStorage = () => {
+    employeePayrollList = localStorage.getItem('EmployeePayrollList') ?
+        JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+        processEmployeePayrollDataResponse();
+}
+
+const getEmployeePayrollDataFromServer = () => {
+    makeServiceCall("GET", site_properties.server_url, true)
+        .then(responseText => {
+            employeePayrollList = JSON.parse(responseText);
+            processEmployeePayrollDataResponse();
+        })
+        .catch(error => {
+            console.log("GET Error Status: " + JSON.stringify(error));
+            employeePayrollList = [];
+            processEmployeePayrollDataResponse();
+        })
+}
+
+const processEmployeePayrollDataResponse = () => {
     document.querySelector(".emp-count").textContent = employeePayrollList.length;
     createInnerHtml();
     localStorage.removeItem('editEmp');
-});
-
-const getEmployeePayrollDataFromStorage = () => {
-    return localStorage.getItem('EmployeePayrollList') ?
-        JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
 }
+
 
 const createInnerHtml = () => {
     const headerHtml = `
--- js/site_properties.js
+++ js/site_properties.js
@@ -1,4 +1,6 @@
-let links = {
-    homePage = "../pages/home_page.html",
-    payrollPage = "../pages/payroll_form.html"
+const site_properties = {
+    use_local_storage: false,
+    homePage: "../pages/home_page.html",
+    payrollPage: "../pages/payroll_form.html",
+    server_url: "http://localhost:3000/employeePayroll"
 };
